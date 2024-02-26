c0c049f86c12f8b06c49897bb7b51e812c344a96
Gopinath.s <gopinath.s2022cce@sece.ac.in>
2024-02-26 22:52:35 +0530

    profile page completed

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,18 @@ import 'dart:js';
 
 import 'package:flutter/material.dart';
 import 'package:jobportal/component/view/login_view.dart';
+import 'package:jobportal/component/view/profile_view.dart';
 import 'package:jobportal/component/view/search_view.dart';
 import 'package:jobportal/component/view/sign-up_view.dart';
 
 void main() {
   Widget MyApp = MaterialApp(
-    initialRoute: "/search",
+    initialRoute: "/profile",
     routes: {
       "/login" : (context) => const LoginPage(),
       "/signIn" : (context) => const SignInPage(),
-      "/search" : (context) => const SearchPage()
+      "/search" : (context) => const SearchPage(),
+      "/profile" : (context) => const ProfilePage()
      },
   );
   runApp(MyApp);
