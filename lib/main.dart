3094d4837257b3046a1e527a678263c5b3dbb1a5
Gopinath.s <gopinath.s2022cce@sece.ac.in>
2024-02-26 21:05:56 +0530

    SEARCH PAGE COMPLETED

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:jobportal/component/view/login_view.dart';
+import 'package:jobportal/component/view/search_view.dart';
 import 'package:jobportal/component/view/sign-up_view.dart';
 
 void main() {
   Widget MyApp = MaterialApp(
-    initialRoute: "/login",
+    initialRoute: "/search",
     routes: {
       "/login" : (context) => const LoginPage(),
       "/signIn" : (context) => const SignInPage(),
-    },
+      "/search" : (context) => const SearchPage()
+     },
   );
   runApp(MyApp);
 }
