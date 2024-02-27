e08b03ccda00e417baed27a2989dd454df923ecc
Gopinath.s <gopinath.s2022cce@sece.ac.in>
2024-02-27 21:44:51 +0530

    Application tracker completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 
+import 'dart:js';
+
 import 'package:flutter/material.dart';
+import 'package:jobportal/component/view/application_tracker_user.dart';
 import 'package:jobportal/component/view/create_job.dart';
 import 'package:jobportal/component/view/job_list_employee_view.dart';
 import 'package:jobportal/component/view/login_view.dart';
@@ -10,7 +13,7 @@ import 'package:jobportal/component/view/sign-up_view.dart';
 
 void main() {
   Widget myApp = MaterialApp(
-    initialRoute: "/jobListEmployee",
+    initialRoute: "/applicationTracker",
     routes: {
       "/login": (context) => const LoginPage(),
       "/signIn": (context) => const SignInPage(),
@@ -18,7 +21,8 @@ void main() {
       "/profile": (context) => const ProfilePage(),
       "/profileEmployee": (context) => const ProfileEmployeePage(),
       "/createJob": (context) => const CreateJobPage(),
-      "/jobListEmployee" : (context) => const JobListPageForEmployee()
+      "/jobListEmployee" : (context) => const JobListPageForEmployee(),
+      "/applicationTracker" : (context) => const ApplicationTrackerUser()
     },
   );
   runApp(myApp);
