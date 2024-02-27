37e26b7550f2a81d6b45cde0a1f01909f986a169
Gopinath.s <gopinath.s2022cce@sece.ac.in>
2024-02-27 21:24:24 +0530

    job list for employee completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+
 import 'package:flutter/material.dart';
 import 'package:jobportal/component/view/create_job.dart';
+import 'package:jobportal/component/view/job_list_employee_view.dart';
 import 'package:jobportal/component/view/login_view.dart';
 import 'package:jobportal/component/view/profile_employer_view.dart';
 import 'package:jobportal/component/view/profile_view.dart';
@@ -8,7 +10,7 @@ import 'package:jobportal/component/view/sign-up_view.dart';
 
 void main() {
   Widget myApp = MaterialApp(
-    initialRoute: "/createJob",
+    initialRoute: "/jobListEmployee",
     routes: {
       "/login": (context) => const LoginPage(),
       "/signIn": (context) => const SignInPage(),
@@ -16,6 +18,7 @@ void main() {
       "/profile": (context) => const ProfilePage(),
       "/profileEmployee": (context) => const ProfileEmployeePage(),
       "/createJob": (context) => const CreateJobPage(),
+      "/jobListEmployee" : (context) => const JobListPageForEmployee()
     },
   );
   runApp(myApp);
