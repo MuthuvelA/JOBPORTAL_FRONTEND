e57995e1f0f970b895b1b0ed2bfd17d9335fe3a3
MuthuvelA <muthuveldiary@gmail.com>
2024-02-29 14:47:33 +0530

    connected login and signup with validation

--- lib/component/view/signupUser.dart
+++ lib/component/view/signupUser.dart
@@ -1,4 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../view_model/signupmodel.dart';
+import 'login_view.dart';
 import 'package:jobportal/component/view/profile_view.dart';
 
 class SignInPage extends StatefulWidget {
@@ -9,13 +15,15 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
-
+  TextEditingController email = TextEditingController();
+  TextEditingController password = TextEditingController();
+  TextEditingController username= TextEditingController();
   GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
+        title: const Text(
           "JOBPORTAL",
           style: TextStyle(
             fontSize: 25,
@@ -47,6 +55,7 @@ class _SignInPageState extends State<SignInPage> {
               ),
               //color: Colors.white,
               child: TextFormField(
+                controller: username,
                 decoration: InputDecoration(
                     hintText: "Email Full Name",
                     hintStyle: TextStyle(fontFamily: "Poppins", fontWeight: FontWeight.w700),
@@ -74,6 +83,7 @@ class _SignInPageState extends State<SignInPage> {
               ),
               //color: Colors.white,
               child: TextFormField(
+                controller: email,
                 decoration: InputDecoration(
                     hintText: "Email Address",
                     hintStyle: TextStyle(fontFamily: "Poppins", fontWeight: FontWeight.w700),
@@ -96,6 +106,7 @@ class _SignInPageState extends State<SignInPage> {
               ),
               //color: Colors.white,
               child: TextFormField(
+                controller: password,
                 obscureText: true,
                 decoration: InputDecoration(
                     hintText: "Enter Password",
@@ -120,7 +131,8 @@ class _SignInPageState extends State<SignInPage> {
             const SizedBox(height: 40,),
             ElevatedButton(
                 onPressed: (){
-                    _submitForm();
+                  //  _submitForm();
+                  validateUser(username,email,password);
                 },
                 child: Text("Sign In",style: TextStyle(color: Colors.white,fontSize: 16,fontFamily: 'Popppins-bold'),),
               style: ButtonStyle(
@@ -160,7 +172,10 @@ class _SignInPageState extends State<SignInPage> {
                   actions: [
                     ElevatedButton(
                       onPressed: (){
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => const ProfilePage()));
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => const LoginPage()),
+                        );
                       },
                       child: Text("OK",style: TextStyle(color: Colors.white,fontSize: 16),),
                       style: ButtonStyle(
--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,29 @@
 import 'dart:js';
 
 import 'package:flutter/material.dart';
+import 'package:jobportal/component/view/application_tracker_user.dart';
+import 'package:jobportal/component/view/create_job.dart';
+import 'package:jobportal/component/view/job_list_employee_view.dart';
 import 'package:jobportal/component/view/login_view.dart';
+import 'package:jobportal/component/view/profile_employer_view.dart';
+import 'package:jobportal/component/view/profile_view.dart';
 import 'package:jobportal/component/view/search_view.dart';
 import 'package:jobportal/component/view/sign-up_view.dart';
 
 void main() {
-  Widget MyApp = MaterialApp(
-    initialRoute: "/search",
+  Widget myApp = MaterialApp(
+    initialRoute: "/entry",
     routes: {
-      "/login" : (context) => const LoginPage(),
-      "/signIn" : (context) => const SignInPage(),
-      "/search" : (context) => const SearchPage()
-     },
+      "/login": (context) => const LoginPage(),
+      "/signIn": (context) => const SignInPage(),
+      "/search": (context) => const SearchPage(),
+      "/profile": (context) => const ProfilePage(),
+      "/profileEmployee": (context) => const ProfileEmployeePage(),
+      "/createJob": (context) => const CreateJobPage(),
+      "/jobListEmployee" : (context) => const JobListPageForEmployee(),
+      "/applicationTracker" : (context) => const ApplicationTrackerUser(),
+      "/entry" : (context) => const EntryPage()
+    },
   );
-  runApp(MyApp);
+  runApp(myApp);
 }
