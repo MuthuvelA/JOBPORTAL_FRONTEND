6a0236482167f956b1bdfb441e2a447aca6a0d33
Gopinath.s <gopinath.s2022cce@sece.ac.in>
2024-03-01 21:31:58 +0530

    updated entry and profile_employer_view.dart

--- lib/component/view/profile_employer_view.dart
+++ lib/component/view/profile_employer_view.dart
@@ -8,6 +8,13 @@ class ProfileEmployeePage extends StatefulWidget {
 }
 
 class _ProfileEmployeePageState extends State<ProfileEmployeePage> {
+
+  TextEditingController companyNameController = TextEditingController();
+  TextEditingController employeeNameController = TextEditingController();
+  TextEditingController emailIdController = TextEditingController();
+  TextEditingController mobileController = TextEditingController();
+  TextEditingController companyAddressController = TextEditingController();
+  TextEditingController designationController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,84 +35,36 @@ class _ProfileEmployeePageState extends State<ProfileEmployeePage> {
         children: [
           Center(child: profilePage()),
           SizedBox(height: 30,),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "Company Name",
-              style: TextStyle(fontSize: 17, fontFamily: "Poppins-bold"),
-            ),
-          ),
-          Container(
-            width:400,
-            child: TextField(
-            ),
-          ),
+          textField("Company Name",companyNameController),
           SizedBox(height: 20,),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "Employee Name",
-              style: TextStyle(fontSize: 17, fontFamily: "Poppins-bold"),
-            ),
-          ),
-          Container(
-            width: 400,
-            child: TextField(
-            ),
-          ),
+          textField("Employee Name",companyNameController),
           SizedBox(height: 20,),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "Email Id",
-              style: TextStyle(fontSize: 17, fontFamily: "Poppins-bold"),
-            ),
-          ),
-          Container(
-            width: 400,
-            child: TextField(
-            ),
-          ),
+          textField("Email ID",emailIdController),
           SizedBox(height: 20,),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "Mobile",
-              style: TextStyle(fontSize: 17, fontFamily: "Poppins-bold"),
-            ),
-          ),
-          Container(
-            width:400,
-            child: TextField(
-            ),
-          ),
+          textField("Mobile",mobileController),
           SizedBox(height: 20,),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "Company Adress",
-              style: TextStyle(fontSize: 17, fontFamily: "Poppins-bold"),
-            ),
-          ),
           Container(
-            width:400,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10)
+            ),
             child: TextField(
+              controller: companyAddressController,
+              decoration: InputDecoration(
+                hintText: "Company Address",
+                hintStyle: TextStyle(fontFamily: "Poppins-bold", fontWeight: FontWeight.w700),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: const BorderSide(
+                        color: Colors.blue, width: 2
+                    )
+                ),
+              ),
               maxLines: 3,
+              // controller: mobileController,
             ),
           ),
           SizedBox(height: 20,),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "Designation",
-              style: TextStyle(fontSize: 17, fontFamily: "Poppins-bold"),
-            ),
-          ),
-          Container(
-            width:400,
-            child: TextField(
-            ),
-          ),
+          textField("Designation",designationController),
           SizedBox(height: 30,),
           ElevatedButton(
             onPressed: () {},
@@ -130,8 +89,29 @@ class _ProfileEmployeePageState extends State<ProfileEmployeePage> {
           width: 3,  // Adjust the border width as needed
         ),
       ),
-      child: CircleAvatar(
+      child: const CircleAvatar(
         radius: 75,
+      ),
+    );
+  }
+
+  Widget textField(String hint,TextEditingController controller){
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10)
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: hint,
+          hintStyle: TextStyle(fontFamily: "Poppins-bold", fontWeight: FontWeight.w700),
+          border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10),
+              borderSide: const BorderSide(
+                  color: Colors.blue, width: 2
+              )
+          ),
+        ),
+        controller: controller,
       ),
     );
   }
