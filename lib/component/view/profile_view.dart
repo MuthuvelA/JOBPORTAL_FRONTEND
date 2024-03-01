12986b0af099834e6be7f8380748aa06c9fe5e60
Gopinath.s <gopinath.s2022cce@sece.ac.in>
2024-03-01 15:55:46 +0530

    profile page for user adjustment completed

--- lib/component/view/profile_view.dart
+++ lib/component/view/profile_view.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:jobportal/component/view/search_view.dart';
+import 'package:jobportal/component/view_model/profileviewModel.dart'; // Import your ProfileViewModel class
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController mobileController = TextEditingController();
+  TextEditingController pincodeController = TextEditingController();
+  TextEditingController stateController = TextEditingController();
+  TextEditingController currentWorkController = TextEditingController();
 
-  String ? dropDownValue;
+  String? dropDownValue;
   final List<String> _skillSet = [
     'C',
     'C++',
@@ -24,156 +31,204 @@ class _ProfilePageState extends State<ProfilePage> {
     'Ruby',
     'PHP',
   ];
+
+  // Initialize ProfileViewModel
+  final ProfileViewModel _profileViewModel = ProfileViewModel();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("PROFILE",style: TextStyle(fontFamily: "Poppins-bold",fontSize: 20),),
+        title: Text(
+          "PROFILE",
+          style: TextStyle(fontFamily: "Poppins-bold", fontSize: 20),
+        ),
         backgroundColor: Colors.pink.shade500,
       ),
       body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: bodyPartOfProfile()
-      ),
-    );
-  }
-  Widget bodyPartOfProfile(){
-    return Padding(
-      padding: const EdgeInsets.only(left: 50,right: 50),
-      child: Column(
-        children: [
-          Center(child: profilePage()),
-          SizedBox(height: 30,),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "Name",
-              style: TextStyle(fontSize: 17, fontFamily: "Poppins-bold"),
-            ),
-          ),
-          Container(
-            width: 400,
-            child: TextField(
-            ),
-          ),
-          SizedBox(height: 20,),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "Email Id",
-              style: TextStyle(fontSize: 17, fontFamily: "Poppins-bold"),
-            ),
-          ),
-          Container(
-            width: 400,
-            child: TextField(
-            ),
-          ),
-          SizedBox(height: 20,),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "Mobile",
-              style: TextStyle(fontSize: 17, fontFamily: "Poppins-bold"),
-            ),
-          ),
-          Container(
-            width:400,
-            child: TextField(
-            ),
-          ),
-          SizedBox(height: 20,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.only(left: 50, right: 50),
+          child: Column(
             children: [
-              Text(
-                "Pincode",
-                style: TextStyle(fontSize: 17, fontFamily: "Poppins-bold"),
-              ),
-              Text(
-                "State",
-                style: TextStyle(fontSize: 17, fontFamily: "Poppins-bold"),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
+              Center(child: profilePage()),
+              SizedBox(height: 30),
               Container(
-                width: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                ),
                 child: TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(
+                    hintText: "Enter Name",
+                    hintStyle: TextStyle(fontFamily: "Poppins-bold", fontWeight: FontWeight.w700),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                            color: Colors.blue, width: 2
+                        )
+                    ),
+                  ),
                 ),
               ),
+              SizedBox(height: 20),
               Container(
-                width: 100,
+                width: 400,
                 child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Enter Email",
+                    hintStyle: TextStyle(fontFamily: "Poppins-bold", fontWeight: FontWeight.w700),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                            color: Colors.blue, width: 2
+                        )
+                    ),
+                  ),
+                  controller: emailController,
                 ),
               ),
-            ],
-          ),
-          SizedBox(height: 20,),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "Currently pursuing",
-              style: TextStyle(fontSize: 17, fontFamily: "Poppins-bold"),
-            ),
-          ),
-          Container(
-            width:400,
-            child: TextField(
-            ),
-          ),
-          SizedBox(height: 20,),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "Add your skill set",
-              style: TextStyle(fontSize: 17, fontFamily: "Poppins-bold"),
-            ),
-          ),
-          DropdownButton<String>(
-            icon: Icon(Icons.arrow_drop_down_rounded),
-            iconSize: 20,
-            isExpanded: true,
-            value: dropDownValue,
-            elevation: 16,
-            onChanged: (String? newValue) {
-              setState(() {
-                dropDownValue = newValue;
-              });
-            },
-            items: _skillSet.map<DropdownMenuItem<String>>((String value) {
-               return DropdownMenuItem<String>(
-               value: value,
-                  child: Text(value),
+              SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10)
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: "Enter mobile",
+                    hintStyle: TextStyle(fontFamily: "Poppins-bold", fontWeight: FontWeight.w700),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                            color: Colors.blue, width: 2
+                        )
+                    ),
+                  ),
+                  controller: mobileController,
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10)
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: "Enter PINCODE",
+                    hintStyle: TextStyle(fontFamily: "Poppins-bold", fontWeight: FontWeight.w700),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                            color: Colors.blue, width: 2
+                        )
+                    ),
+                  ),
+                  controller: mobileController,
+                ),
+              ),
+              SizedBox(height: 20,),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10)
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: "Enter STATE",
+                    hintStyle: TextStyle(fontFamily: "Poppins-bold", fontWeight: FontWeight.w700),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                            color: Colors.blue, width: 2
+                        )
+                    ),
+                  ),
+                  controller: mobileController,
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10)
+                ),
+                child: TextField(
+                  decoration: InputDecoration(
+                      hintText: "Currency pursuing",
+                    hintStyle: TextStyle(fontFamily: "Poppins-bold", fontWeight: FontWeight.w700),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: const BorderSide(
+                            color: Colors.blue, width: 2
+                        )
+                    ),
+                  ),
+                  controller: mobileController,
+                ),
+              ),
+              SizedBox(height: 20),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Add your skill set",
+                  style: TextStyle(fontSize: 17, fontFamily: "Poppins-bold"),
+                ),
+              ),
+              DropdownButton<String>(
+                icon: Icon(Icons.arrow_drop_down_rounded),
+                iconSize: 20,
+                isExpanded: true,
+                value: dropDownValue,
+                elevation: 16,
+                onChanged: (String? newValue) {
+                  setState(() {
+                    dropDownValue = newValue;
+                  });
+                },
+                items: _skillSet.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
                   );
                 }).toList(),
               ),
-          SizedBox(height: 30,),
-          ElevatedButton(
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => const SearchPage()));
-            },
-            child: Text("Save", style: TextStyle(color: Colors.white, fontSize: 16.0)),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.pink.shade500,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16.0),
+              SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  _saveProfile(); // Call function to save profile
+                },
+                child: Text("Save", style: TextStyle(color: Colors.white, fontSize: 16.0)),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.pink.shade500,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
+                ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
-  Widget profilePage(){
+
+  void _saveProfile() {
+    _profileViewModel.saveProfile(
+      name: nameController.text,
+      email: emailController.text,
+      mobile: mobileController.text,
+      pincode: pincodeController.text,
+      state: stateController.text,
+      currentWork: currentWorkController.text,
+      selectedSkill: dropDownValue,
+    );
+  }
+
+  Widget profilePage() {
     return Container(
       decoration: BoxDecoration(
         shape: BoxShape.circle,
         border: Border.all(
-          color: Colors.white54,  // Change the border color as needed
-          width: 3,  // Adjust the border width as needed
+          color: Colors.white54,
+          width: 3,
         ),
       ),
       child: CircleAvatar(
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:js';
 import 'package:flutter/material.dart';
 import 'package:jobportal/component/view/application_tracker_user.dart';
 import 'package:jobportal/component/view/create_job.dart';
+import 'package:jobportal/component/view/entry.dart';
 import 'package:jobportal/component/view/job_list_employee_view.dart';
 import 'package:jobportal/component/view/login_view.dart';
 import 'package:jobportal/component/view/profile_employer_view.dart';
